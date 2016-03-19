eb1c8a49dca27765b2f9a7805bb18262085414d5
arnaud huguenin <hhuguenin.arnaud@gmail.com>
2016-03-19 17:16:25 +0100

    model fixed

--- Gemfile
+++ Gemfile
@@ -24,9 +24,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Elastic Search
-gem 'elasticsearch-model'
-
 gem 'elasticsearch-rails'
+
+gem 'elasticsearch-model'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,21 @@
-require 'elasticsearch/model'
+# require 'elasticsearch/model'
 
 class Article < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
+
+ 	# def self.search(query)
+  # 	__elasticsearch__.search(
+  #   	{
+  #     	query: {
+  #       	multi_match: {
+  #         		query: query,
+  #         		fields: ['title^10', 'text']
+  #       	}
+  #     	}
+  #   }
+  # 	)
+ 	# end
 end
-Article.import # for auto sync model with elastic search
+
+Article.import force: true # for auto sync model with elastic search
